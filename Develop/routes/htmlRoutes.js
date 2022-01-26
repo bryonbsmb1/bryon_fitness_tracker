f5a649e9f27b54c47407111a56ac4ebbfb7cd0a6
bryonbsmb1 <87393401+bryonbsmb1@users.noreply.github.com>
2022-01-25 19:36:41 -0500

    html routes setup in js file, dirnames set

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -2,5 +2,19 @@ const express = require("express");
 
 const path = require("path");
 
-const router = require("express"),
+const router = require("express").Router();
 
+router.get ("/", (reg, res) => {
+
+    //setup dirname with double underscores b/c they are global objects per Node.js 
+
+    res.sendFile (path.join(__dirname, "../public/index.html"));
+});
+router.get ("/exercise", (req,res) => {
+    res.sendFile (path.join (__dirname, "../exercise/index.html"));
+});
+router.get ("/stats", (req,res) => {
+    res.sendFile (path.join (__dirname, "../stats/index.html"));
+});
+
+module.exports = router;
